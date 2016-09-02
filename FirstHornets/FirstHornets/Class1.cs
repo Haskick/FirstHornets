c85d66352328e99ce9080fc1df6d5feed43873ba
Haski <gamil007@gmail.com>
2016-09-02 10:19:56 +0200

    second

--- FirstHornets/FirstHornets/Class1.cs
+++ FirstHornets/FirstHornets/Class1.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstHornets
-{
-    public class Class1
-    {
+namespace FirstHornets {
+    public class Class1 {
     }
 }
